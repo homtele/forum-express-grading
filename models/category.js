f3026ace0d89019bd68361e1780dedb330382a1e
HY <homtele@gmail.com>
2022-02-09 21:40:07 +0800

    feat: add delete all restaurants belonging to one category function

--- models/category.js
+++ models/category.js
@@ -10,7 +10,11 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate (models) {
-      Category.hasMany(models.Restaurant, { foreignKey: 'categoryId' })
+      Category.hasMany(models.Restaurant, {
+        foreignKey: 'categoryId',
+        onDelete: 'CASCADE',
+        hooks: true
+      })
     }
   };
   Category.init({
