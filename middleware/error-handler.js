40ecd582e3a500d49c5d77b492485a664fa5f088
HY <homtele@gmail.com>
2022-02-14 00:53:43 +0800

    feat: add api error handler & delete restaurant api

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,3 +1,16 @@
+const apiErrorHandler = (err, req, res, next) => {
+  if (err instanceof Error) {
+    res.status(500).json({
+      status: 'errer',
+      message: `${err.name}: ${err.message}`
+    })
+  } else {
+    res.status(500).json({
+      status: 'errer',
+      message: `${err}`
+    })
+  }
+}
 const generalErrorHandler = (err, req, res, next) => {
   if (err instanceof Error) {
     req.flash('error_messages', `${err.name}: ${err.message}`)
@@ -8,5 +21,6 @@ const generalErrorHandler = (err, req, res, next) => {
   next(err)
 }
 module.exports = {
+  apiErrorHandler,
   generalErrorHandler
 }
--- services/admin-services.js
+++ services/admin-services.js
@@ -23,6 +23,16 @@ const adminServices = {
         })
       })
       .catch(err => cb(err))
+  },
+  deleteRestaurant: (req, cb) => {
+    Restaurant
+      .findByPk(req.params.id)
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.destroy()
+      })
+      .then(deletedRestaurant => cb(null, deletedRestaurant))
+      .catch(err => cb(err))
   }
 }
 module.exports = adminServices
