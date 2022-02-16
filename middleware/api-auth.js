8a61823f64332ec294cf8e879ef680a46edd12d2
HY <homtele@gmail.com>
2022-02-16 23:28:37 +0800

    feat: modify api unauthorize response

--- middleware/api-auth.js
+++ middleware/api-auth.js
@@ -1,9 +1,15 @@
 const passport = require('../config/passport')
 
-const authenticated = passport.authenticate('jwt', { session: false })
+const authenticated = (req, res, next) => {
+  passport.authenticate('jwt', { session: false }, (err, user) => {
+    if (err) return next(err)
+    if (!user) return res.json({ status: 'error', message: 'unauthorized' })
+    next()
+  })(req, res, next)
+}
 const authenticatedAdmin = (req, res, next) => {
-  if (req.user && req.user.isAdmin) return next()
-  return res.status(403).json({ status: 'error', message: 'permission denied' })
+  if (!req.user || !req.user.isAdmin) return res.status(403).json({ status: 'error', message: 'permission denied' })
+  next()
 }
 module.exports = {
   authenticated,
